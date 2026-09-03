a8f89b4fef2231dc134a8f7846d830f7c78503da
sanjayrockerz <myteamcreations09@gmail.com>
2026-09-04 00:22:09 +0530

    feat: add progressive web app support

---
+++ jj-signature-billing-app/public/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'jj-signature-shell-v1'
+const SHELL_ASSETS = ['/', '/index.html', '/manifest.webmanifest', '/jj-signature-logo.png']
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(SHELL_ASSETS))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone()
+          void caches.open(CACHE_NAME).then(cache => cache.put('/index.html', copy))
+          return response
+        })
+        .catch(() => caches.match('/index.html'))
+    )
+    return
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      if (response.ok) {
+        const copy = response.clone()
+        void caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy))
+      }
+      return response
+    }))
+  )
+})
